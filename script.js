2d1488807c0ff65fd93a674d74a19c0f169ca886
sethjeremiah <seth.abrahamj@gmail.com>
2026-09-04 12:23:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("A little something for you.");
+document.addEventListener("DOMContentLoaded", () => {
+
+    const flowers = document.querySelectorAll(".flower");
+
+    flowers.forEach((flower) => {
+        flower.addEventListener("animationend", () => {
+            flower.classList.add("alive");
+        });
+    });
+
+});
